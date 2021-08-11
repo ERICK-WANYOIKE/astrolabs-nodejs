1d67be81eeca535a503a653260d869d07d68e29f
Apple <apple@Apples-MacBook-Pro-526.local>
2021-08-11 23:54:31 +0400

    make it better

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ mongoose
 server.get(
     '/',                        // http://localhost:3001/
     (req, res) => { 
-        res.send("<html><head><title>Home</title></head><body><h1>Welcome to Dany's Website</h1></body></html>")
+        res.send("<html><head><title>Home</title></head><body><h1>Welcome to ERICKS Website</h1></body></html>")
     }
 );
 
